f06216a831e55c2eaab5bf53f5d38707c275674a
Tauassar <tatiyev.tauassar@gmail.com>
2025-12-10 01:03:29 +0500

    fix: remove unnecessary UndefinedNodeError exception

--- src/libs/dag-engine/src/dag_engine/core/exceptions.py
+++ src/libs/dag-engine/src/dag_engine/core/exceptions.py
@@ -4,8 +4,4 @@ class DagEngineException(Exception):
 
 
 class DagValidationError(DagEngineException):
-    ...
-
-
-class UndefinedNodeError(DagEngineException):
-    ...
+    """Occurs when a DAG Validation fails or DAG construction fails."""
